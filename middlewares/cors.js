24e9bde304df674f70984742905f96ca4e179817
Dimitar Vlahov <dimitarvlahov24@gmail.com>
2023-04-06 02:55:31 +0300

    .

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,11 +1,13 @@
 module.exports = () => (req, res, next) => {
   const corsWhiteList = [
-    'https://aniline.vercel.app/',
-    'https://bulgarian-drivers.vercel.app/',
+    'https://aniline.vercel.app',
+    'https://bulgarian-drivers.vercel.app',
     'http://localhost:3000'
   ];
 
+  console.log(corsWhiteList.indexOf(req.headers.origin));
   if (corsWhiteList.indexOf(req.headers.origin) !== -1) {
+    console.log(req.headers.origin);
     res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header('Access-Control-Allow-Headers', 'Content-Type, X-Authorization, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,');
